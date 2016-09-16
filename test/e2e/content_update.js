d3fa1c3e8d4dbe958ab626e88b1a1bf0e0fc1879
Francisco Vallarino <fjvallarino@gmail.com>
2016-09-16 20:46:02 -0300

    Add content update e2e test case

---
+++ test/e2e/content_update.js
@@ -0,0 +1,35 @@
+const assert = require("assert"),
+      request = require("request"),
+      serverKey = process.env.SERVERKEY,
+      baseUrl = process.env.SERVER_URL || "https://display-messaging.risevision.com",
+      serverUrl = baseUrl + ":3001",
+      clientUrl = baseUrl + ":3000",
+      wsClient = require("../ws-client.js");
+
+describe("Content update", function() {
+  this.timeout(5000);
+
+  it("relays a content update message to a display", ()=>{
+    let displayId = String(Math.random()),
+        displayUrl = clientUrl + "?displayId=" + displayId,
+        fakeDisplay = wsClient.createClient(displayUrl),
+        contentUpdateUrl = serverUrl + "?sk=" + serverKey + "&did=" + displayId + "&msg=content_update";
+
+    return new Promise((res)=>{
+      fakeDisplay.on("open", ()=>{
+        fakeDisplay.on("data", (data)=>{
+          if (data.msg === "content_update" && data.displayId === displayId) {
+            fakeDisplay.end();
+            res();
+          }
+        });
+
+        request(contentUpdateUrl, (err, res, body)=>{
+          if(err || res.statusCode !== 200) {
+            console.log("Content update request error", err || res.statusCode);
+          }
+        });
+      });
+    });
+  });
+});
